371c6979b4186421c1546c7e5b41725651984ca9
Andrew Linzie <andrewlinzie@Andrews-MacBook-Air.local>
2025-12-07 16:02:46 -0600

    Fix API routing to backend via /api/

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-const API_URL = "http://devops-app-alb-417544994.us-east-2.elb.amazonaws.com/api";
+const API_URL = "http://devops-app-alb-417544994.us-east-2.elb.amazonaws.com/api/";
 export default API_URL;
