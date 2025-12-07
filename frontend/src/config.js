c79ac63f0bb312f4b540b04da8690d8121b78dbf
Andrew Linzie <andrewlinzie@Andrews-MacBook-Air.local>
2025-12-07 01:49:00 -0600

    Fix API URL for production ALB

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,2 @@
-const API_URL = "http://devops-app-alb-417544994.us-east-2.elb.amazonaws.com/";
-
+const API_URL = "http://devops-app-alb-417544994.us-east-2.elb.amazonaws.com/api";
 export default API_URL;
