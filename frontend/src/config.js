3c06cc39cf34dd690831ea87bb99b05e568c1585
Andrew Linzie <andrewlinzie@Andrews-MacBook-Air.local>
2025-12-09 00:25:05 -0600

    Fix frontend API_URL

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
-const API_URL = "http://alb20251209052722042900000005-1512177750.us-east-2.elb.amazonaws.com/";
+const API_URL = "http://alb20251209052722042900000005-1512177750.us-east-2.elb.amazonaws.com/api/";
 export default API_URL;
